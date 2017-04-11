e030363117a3fc14c1ad82f3d5babe4da5b3b724
vinhtc2000 <vinhtc2000@gmail.com>
2017-04-11 20:24:09 +0700

    add html report

--- config/FullRegressionTest2.js
+++ config/FullRegressionTest2.js
@@ -1,25 +1,49 @@
-var verTestReport = require('protractor-build-verification-testreport');
 var env = require('../common/Constant.js');
-console.log(env.GlobalVars.seleniumServer);
+var HtmlScreenshotReporter = require('../resources/node_modules/protractor-jasmine2-screenshot-reporter');
 
-var config  = {
-	seleniumAddress: env.GlobalVars.seleniumServer,
-	//directConnect: true,  
-	specs: ['tm1.js'],
+var reporter = new HtmlScreenshotReporter({
+	userCss : '../example-custom.css',
+	dest : 'reports/tmp/screenshots',
+	filename : 'my-report.html',
+});
+
+exports.config = {
+	// Setup the report before any tests start
+	beforeLaunch : function() {
+		return new Promise(function(resolve) {
+			reporter.beforeLaunch(resolve);
+		});
+		process.on('uncaughtException', function() {
+			reporter.jasmineDone();
+			reporter.afterLaunch();
+		});
+	},
+
+	seleniumAddress : env.GlobalVars.seleniumServer,
+	// directConnect: true,
+	specs : [ '../tests/tm1.js' ],
+	// multiCapabilities: [{
+	// 'browserName': 'chrome'
+	// }, {
+	// 'browserName': 'firefox'
+	// }],
+	params : {
+		login : {
+			user : 'Jane',
+			password : '1234'
+		},
+		constant : env.GlobalVars
+	},
 	
-//	multiCapabilities: [{
-//	    'browserName': 'chrome'
-//	  }, {
-//	    'browserName': 'firefox'
-//	  }],
-	params: {
-		login: {
-			user: 'Jane',
-			password: '1234'
-	    	},
-	    constant: env.GlobalVars
-	  	},
-	onPrepare: verTestReport.onPrepare
+	// Assign the test reporter to each running instance
+	onPrepare : function() {
+		jasmine.getEnv().addReporter(reporter);
+	},
+	
+	// Close the report after all tests finish
+	afterLaunch : function(exitCode) {
+		return new Promise(function(resolve) {
+			reporter.afterLaunch(resolve.bind(this, exitCode));
+		});
+	}
 };
-exports.config = config;
-		
--- tests/tm1.js
+++ tests/tm1.js
@@ -6,18 +6,36 @@ describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
 	console.log(browser.params.constant.password); 
 	browsers.launch('https://angularjs.org');
-	browsers.close();
+//	browsers.close();
 	
-//    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-//    element(by.css('[value="add"]')).click();
-//
-//    var todoList = element.all(by.repeater('todo in todoList.todos'));
-//    expect(todoList.count()).toEqual(3);
-//    expect(todoList.get(2).getText()).toEqual('write first protractor test');
-//
-//    // You wrote your first test, cross it off the list
-//    todoList.get(2).element(by.css('input')).click();
-//    var completedAmount = element.all(by.css('.done-true'));
-//    expect(completedAmount.count()).toEqual(2);
+    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+    element(by.css('[value="add"]')).click();
+
+    var todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(3);
+    expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+    // You wrote your first test, cross it off the list
+    todoList.get(2).element(by.css('input')).click();
+    var completedAmount = element.all(by.css('.done-true'));
+    expect(completedAmount.count()).toEqual(2);
   });
+  
+  it('should add a todo 2', function() {
+		console.log(browser.params.constant.password); 
+		browsers.launch('https://angularjs.org');
+//		browsers.close();
+		
+	    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+	    element(by.css('[value="add"]')).click();
+
+	    var todoList = element.all(by.repeater('todo in todoList.todos'));
+	    expect(todoList.count()).toEqual(3);
+	    expect(todoList.get(2).getText()).toEqual('write second protractor test');
+
+	    // You wrote your first test, cross it off the list
+	    todoList.get(2).element(by.css('input')).click();
+	    var completedAmount = element.all(by.css('.done-true'));
+	    expect(completedAmount.count()).toEqual(2);
+	  });
 });
